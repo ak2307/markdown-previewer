cf05beada2f180fc60a1b7c236922b20934e1aaa
Abhishek Kapil <kapil_abhishek@rocketmail.com>
2020-09-21 19:23:31 -0700

    Updates

---
+++ precache-manifest.cafb83d19dc1ba628723446508bd2bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9044c6aa9cc756a494bdc8c79faa8a",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "769db839dbefa9b6e5b6",
+    "url": "/markdown-previewer/static/css/main.429c4443.chunk.css"
+  },
+  {
+    "revision": "5a970d6de047af96c52a",
+    "url": "/markdown-previewer/static/js/2.219dc719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.219dc719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769db839dbefa9b6e5b6",
+    "url": "/markdown-previewer/static/js/main.aed88a50.chunk.js"
+  },
+  {
+    "revision": "9cf44a11e2b2e76dc87a",
+    "url": "/markdown-previewer/static/js/runtime-main.68b74cc5.js"
+  }
+]);
