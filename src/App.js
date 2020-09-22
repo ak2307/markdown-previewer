5feeda496dd32659b1bec5ba0296412cf3276c44
Abhishek Kapil <kapil_abhishek@rocketmail.com>
2020-09-21 19:19:44 -0700

    initial release

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+let marked = require("marked");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+    super();
+        this.state = {
+        markdown : "# Heading\n## Sub-heading\n\n[This is a link.](https://www.freecodecamp.org)\n\n`This is inline code.`\n\n```\nThis is a code block.\n```\n\nThis is a list:\n* item 1\n* item 2\n* item 3\n\n> This is a blockquote.\n\n![Image](https://raw.githubusercontent.com/Johnny2136/FCC-Projects/master/images/images.png)\n\n**This is bolded text.**'"
+    }
+    this.handleChange =this.handleChange.bind(this);
+}
+    handleChange(event) {
+        this.setState ({markdown: event.target.value});
+    }
+
+    render () {
+        const renderer = new marked.Renderer();
+        renderer.link = (href, title, text) => `<a href "${href}" target ="_blank">${text}</a>`
+        return (
+            <div>
+                <h1 className="heading">Markdown Editor/Previewer</h1>
+                <div className="container">
+                    <div id="editor-window" title="editor window" className="outer-window">
+                        <h1 className="window-heading">Markdown Input</h1>
+                        <textarea 
+                            placeholder="Enter Markdown" 
+                            rows="40" 
+                            value={this.state.markdown} 
+                            id="editor" 
+                            onChange= {(event) => this.handleChange(event)}>
+                        </textarea>
+                    </div>
+                    <div title="previewer window" className="outer-window">
+                        <h1 className="window-heading">Markdown Output</h1>
+                        <div id="preview" dangerouslySetInnerHTML = {{ __html: marked(this.state.markdown, {breaks: true, renderer})}}>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
+
 }
 
 export default App;
